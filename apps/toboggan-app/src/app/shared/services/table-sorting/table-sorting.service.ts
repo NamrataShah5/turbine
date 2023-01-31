32f109cc9a3d8a81e6ee4a786060460c3db22e3a
Shafi Shamsudheen <102204577+shafi-sadaindia@users.noreply.github.com>
2023-01-31 19:32:43 +0530

    Table sorting issue (#155)

--- apps/toboggan-app/src/app/shared/services/table-sorting/table-sorting.service.ts
+++ apps/toboggan-app/src/app/shared/services/table-sorting/table-sorting.service.ts
@@ -18,12 +18,12 @@ export class TableSortingService {
       if (
         columnDisplayMetadata[i].sort &&
         columnDisplayMetadata[i].sort !== TableColumnSortStateEnum.None
-        ){
-          sortColumnDataKey = columnDisplayMetadata[i].dataKey;
-        if (columnDisplayMetadata[i].sort === TableColumnSortStateEnum.Ascending){
+      ) {
+        sortColumnDataKey = columnDisplayMetadata[i].dataKey;
+        if (columnDisplayMetadata[i].sort === TableColumnSortStateEnum.Ascending) {
           return { sortColumnDataKey, sortDirectionCoefficient: 1 };
         }
-        if (columnDisplayMetadata[i].sort === TableColumnSortStateEnum.Descending){
+        if (columnDisplayMetadata[i].sort === TableColumnSortStateEnum.Descending) {
           return { sortColumnDataKey, sortDirectionCoefficient: -1 };
         }
         break;
@@ -37,14 +37,16 @@ export class TableSortingService {
     b: TableRow,
     sortColumnDataKey: string,
     sortDirectionCoefficient: number
-  ):number {
-      if (a.cellData[sortColumnDataKey] < b.cellData[sortColumnDataKey]) {
-        return -1 * sortDirectionCoefficient;
-      }
-      if (a.cellData[sortColumnDataKey] > b.cellData[sortColumnDataKey]) {
-        return 1 * sortDirectionCoefficient;
-      }
-      return 0;
+  ): number {
+    const aLowerCase = JSON.stringify(a.cellData[sortColumnDataKey]).toLowerCase();
+    const bLowerCase = JSON.stringify(b.cellData[sortColumnDataKey]).toLowerCase();
+    if (aLowerCase < bLowerCase) {
+      return -1 * sortDirectionCoefficient;
+    }
+    if (aLowerCase > bLowerCase) {
+      return 1 * sortDirectionCoefficient;
+    }
+    return 0;
   }
 
   public getSortedData(
@@ -52,6 +54,6 @@ export class TableSortingService {
     sortColumnDataKey: string,
     sortDirectionCoefficient: number
   ) {
-    return data.sort((a, b) => this.sortOnDatakeynCoeff(a,b,sortColumnDataKey,sortDirectionCoefficient));
+    return data.sort((a, b) => this.sortOnDatakeynCoeff(a, b, sortColumnDataKey, sortDirectionCoefficient));
   }
 }
